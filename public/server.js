8855bcc4797e712fef00e6f852597b08dd930b1a
Carlo <pbjunkiep3@yahoo.com>
2020-08-03 10:53:11 +1000

    added post in notes html

--- public/server.js
+++ public/server.js
@@ -13,6 +13,12 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'notes.html'))
+})
+
+//
+
 app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'notes.html'));
 });
